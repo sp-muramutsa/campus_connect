48b3ea410fb2df579acabf685b1499a461ad3150
sandrine Bakuramutsa <sandrinebakuramutsa@sandrines-MacBook-Air.local>
2025-12-11 03:02:31 -0600

    Added createsuperuser command to build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,5 @@ python manage.py migrate
 # Use environment variables for credentials
 python manage.py createsuperuser --noinput --username $SUPERUSER_NAME --email $SUPERUSER_EMAIL || true
 
+# Error happens here:
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$SUPERUSER_NAME').first().set_password('$SUPERUSER_PASSWORD'); User.objects.filter(username='$SUPERUSER_NAME').first().save()" | python manage.py shell
